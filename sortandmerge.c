e78882545f5cf281dfd1ce4669e0862b0cfda655
AKHILA V AUGUSTINE <96070325+AKHILAVAUGUSTIN@users.noreply.github.com>
2022-03-12 21:19:31 +0530

    Update sortandmerge.c

--- sortandmerge.c
+++ sortandmerge.c
@@ -1,25 +1,4 @@
-sortandmerge.c
-Who has access
-Not shared
-System properties
-Type
-C
-Size
-915 bytes
-Storage used
-915 bytes
-Location
-24/11/2021
-Owner
-me
-Modified
-Dec 5, 2021 by me
-Opened
-8:04 AM by me
-Created
-Dec 5, 2021 with Google Drive Web
-Add a description
-Viewers can download
+
 #include<stdio.h>
 void main()
 {
